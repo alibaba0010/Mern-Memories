a17273a33dd79cb834ee30a8d0c1f051c5042ab0
alibaba0010 <zakariyahali100@gmail.com>
2023-06-12 13:53:33 +0100

    Finishing 1 & 2

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
